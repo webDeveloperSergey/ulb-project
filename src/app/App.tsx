e1c12ae8059855b496e3e0dba4f38f19745320a2
Sergei Kultyshkin <kultyshkin2003@gmail.com>
2025-06-21 22:03:45 +0300

    декомпозировали react router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
-import { Suspense } from 'react';
+import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
@@ -17,12 +15,7 @@ const App = () => {
 
             <button onClick={toggleTheme}>Toggle Theme</button>
 
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
