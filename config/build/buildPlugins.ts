557ff4f12838e3ad2f8a4a61279ed5fb762f1404
Sergei Kultyshkin <kultyshkin2003@gmail.com>
2025-08-21 19:16:35 +0300

    feat(main): добавили hot: true и пакеты HotModuleReplacementPlugin и ReactRefreshWebpackPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,13 +1,19 @@
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import { DefinePlugin, ProgressPlugin, WebpackPluginInstance } from 'webpack'
+import {
+	DefinePlugin,
+	HotModuleReplacementPlugin,
+	ProgressPlugin,
+	WebpackPluginInstance,
+} from 'webpack'
 import { BuildOptions } from './types/config'
 
 export function buildPlugins({
 	paths,
 	isDev,
 }: BuildOptions): WebpackPluginInstance[] {
-	return [
+	const plugins = [
 		new ProgressPlugin(),
 		new HtmlWebpackPlugin({
 			template: paths.html,
@@ -21,4 +27,11 @@ export function buildPlugins({
 			__IS_DEV__: JSON.stringify(isDev),
 		}),
 	]
+
+	if (isDev) {
+		plugins.push(new HotModuleReplacementPlugin())
+		plugins.push(new ReactRefreshWebpackPlugin())
+	}
+
+	return plugins
 }
