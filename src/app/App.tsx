229d121f34917304bb456c9a4cca5c5dd583df83
Sergei Kultyshkin <kultyshkin2003@gmail.com>
2025-08-23 11:34:52 +0300

    add(main): добавили сайдбар на страницу + улучшили верстку

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,22 @@
 import { useTheme } from 'app/providers/ThemeProvider'
-import { AppRouter } from 'app/providers/router'
+import { AppRouter } from 'app/providers/AppRouter'
 import { Suspense } from 'react'
 import { classNames } from 'shared/lib/classNames'
-import { LangSwitcher } from 'widgets/LangSwitcher'
 import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
 import './styles/index.scss'
 
 const App = () => {
-	const { theme, toggleTheme } = useTheme()
+	const { theme } = useTheme()
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
 			<Suspense fallback='loading'>
-				<LangSwitcher />
 				<Navbar />
-				<AppRouter />
+				<div className='content-page'>
+					<Sidebar />
+					<AppRouter />
+				</div>
 			</Suspense>
 		</div>
 	)
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,6 @@
 import { classNames } from 'shared/lib/classNames'
 import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink'
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
+import { LangSwitcher } from 'widgets/LangSwitcher'
 import cls from './Navbar.module.scss'
 
 interface NavbarProps {
@@ -10,7 +10,7 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
 	return (
 		<div className={classNames(cls.Navbar, {}, [className])}>
-			<ThemeSwitcher />
+			<LangSwitcher />
 
 			<div className={cls.links}>
 				<AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>
